f807b397042c484380ffbd3416193f6b1ee04782
Dan <daniel.lauri22@gmail.com>
2023-10-29 10:49:38 +0100

    Lighting/Basic-Lighting

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,9 +1,28 @@
 #version 330 core
-out vec4 FragColor;
+in vec3 vNormal;
+in vec3 vFragPos;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+out vec4 FragColor;
 
 void main() {
-    FragColor = vec4(lightColor * objectColor, 1.0);
+    float specularStrength = 0.5;
+
+    vec3 viewDir = normalize(viewPos - vFragPos);
+    vec3 norm = normalize(vNormal);
+    vec3 lightDir = normalize(lightPos - vFragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    FragColor = vec4(result, 1.0);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,16 +1,16 @@
 #version 330 core
 layout(location = 0) in vec3 aPos;
-
-out vec3 ourColor;
-out vec2 vTexCoord;
-
+layout(location = 1) in vec3 aNormal;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+out vec3 vNormal;
+out vec3 vFragPos;
 
 void main() {
     gl_Position = projection * view * model * vec4(aPos, 1.0f);
-
+    vFragPos = vec3(model * vec4(aPos, 1.0));
+    vNormal = mat3(transpose(inverse(model))) * aNormal;
 }
